35ab85f6dae003d66cc7fe2aeb77d6fabd544b31
payamtajdivand <payamt1919@gmail.com>
2026-02-01 22:08:58 +0330

    different style methods in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import "./App.css";
-import Card from "./Card.jsx";
+import Button from "./Button/Button.jsx";
 
 function App() {
-  return (
-    <>
-      <Card></Card>
-    </>
-  );
+  return <Button />;
 }
 
 export default App;
