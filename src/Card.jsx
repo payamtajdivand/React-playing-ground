271ce01752e3d958b57e102c18f0c6125a12a1d8
payamtajdivand <payamt1919@gmail.com>
2026-02-01 17:50:14 +0330

    create card compoent add and add class and styles

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,10 +2,10 @@ import profilePic from "./assets/profilePic.jpeg";
 
 function Card() {
   return (
-    <div>
-      <img src={profilePic} alt="profile picture"></img>
-      <h2>Mr Nose</h2>
-      <p>I do coding stuff and Im good at it</p>
+    <div className="card">
+      <img className="card-image" src={profilePic} alt="profile picture"></img>
+      <h2 className="card-title">Mr Nose</h2>
+      <p className="card-text">I do coding stuff and Im good at it</p>
     </div>
   );
 }
