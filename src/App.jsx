e85c178f0214f373807756c34006fa3b83f7173f
payamtajdivand <payamt1919@gmail.com>
2026-02-02 14:21:12 +0330

    practice some props and prop types modlues how ever its dead in modern React apps

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import "./App.css";
-import Button from "./Button.jsx";
+import Student from "./Student.jsx";
 
 function App() {
-  return <Button />;
+  return (
+    <>
+      <Student name="Kolah Ghermezi" age={15} isStudent={true} />
+      <Student name="Peasr Khale" age={17} isStudent={false} />
+      <Student name="Gabi" age={30} isStudent={false} />
+      <Student name="Pesar Ama za" age={13} isStudent={true} />
+      <Student name="Agha -e- hamsaye " age={52} isStudent={false} />
+    </>
+  );
 }
 
 export default App;
