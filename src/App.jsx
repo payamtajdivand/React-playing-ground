85dbe9292ab23d0ef3d91b6c3959f71fa98cc232
payamtajdivand <payamt1919@gmail.com>
2026-02-03 18:56:52 +0330

    get list data from app and add category

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import List from "./List.jsx";
 
 function App() {
+  const fruits = [
+    { id: 1, name: "banana", calories: 100 },
+    { id: 2, name: "apple", calories: 220 },
+    { id: 3, name: "orange", calories: 332 },
+    { id: 4, name: "kiwi", calories: 60 },
+    { id: 5, name: "pine apple", calories: 42 },
+  ];
   return (
     <>
-      <p>
-        attention! <br></br>calories are not real
-      </p>
-      <List />
+      <List category="Fruits" items={fruits} />
     </>
   );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,30 +1,30 @@
-function List() {
-  const fruits = [
-    { id: 1, name: "banana", calories: 100 },
-    { id: 2, name: "apple", calories: 220 },
-    { id: 3, name: "orange", calories: 332 },
-    { id: 4, name: "kiwi", calories: 60 },
-    { id: 5, name: "pine apple", calories: 42 },
-  ];
+function List(props) {
   //fruits.sort((a, b) => a.calories - b.calories); //sort numeric lower to higher
   //fruits.sort((a, b) => b.calories - a.calories); //sort numeric higher to lower
   //fruits.sort((a, b) => a.name.localeCompare(b.name)); // sort alphabets a to z
   // fruits.sort((a, b) => b.name.localeCompare(a.name)); // sort alphabets z to a
-  fruits.sort((a, b) => b.calories - a.calories);
+  //fruits.sort((a, b) => b.calories - a.calories);
 
   //const lowCalFruits = fruits.filter((fruit) => fruit.calories < 100); //low cal fruits
   // const highCalFruits = fruits.filter(
   //   (highCalFruit) => highCalFruit.calories >= 100,
   // ); high cal fruits
-
-  const fruitList = fruits.map((fruit) => (
-    <li key={fruit.id}>
-      {fruit.name}: &nbsp;
-      <b>{fruit.calories}</b>
+  const category = props.category;
+  const itemList = props.items;
+  const listItems = itemList.map((item) => (
+    <li key={item.id}>
+      {item.name}: &nbsp;
+      <b>{item.calories}</b>
     </li>
   ));
 
-  return <ol>{fruitList}</ol>;
+  return (
+    <>
+      {" "}
+      <h3>{category}</h3>
+      <ol>{listItems}</ol>
+    </>
+  );
 }
 
 export default List;
