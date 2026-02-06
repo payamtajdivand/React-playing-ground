b1ea38692a282fcacc5e7afb23176a980039baf1
payamtajdivand <payamt1919@gmail.com>
2026-02-06 19:21:52 +0330

    make buttonsfunctional

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -14,9 +14,30 @@ function ToDoList() {
       setNewTask("");
     }
   }
-  function removeTask() {}
-  function moveTaskUp() {}
-  function moveTaskDown() {}
+  function removeTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTask(updatedTasks);
+  }
+  function moveTaskUp(index) {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [
+        updatedTasks[index - 1],
+        updatedTasks[index],
+      ];
+      setTask(updatedTasks);
+    }
+  }
+  function moveTaskDown(index) {
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [
+        updatedTasks[index + 1],
+        updatedTasks[index],
+      ];
+      setTask(updatedTasks);
+    }
+  }
   function handleInputChange(event) {
     setNewTask(event.target.value);
   }
