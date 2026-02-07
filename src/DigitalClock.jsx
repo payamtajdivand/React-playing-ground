5e494b957872b967ba2bbd0da7edf105d4041d2f
payamtajdivand <payamt1919@gmail.com>
2026-02-07 23:59:47 +0330

    add some functions

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -1,10 +1,35 @@
 import React, { useState, useEffect } from "react";
 
 function DigitalClock() {
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(time);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  function padZero(number) {
+    return number < 10 ? "0" : "" + number;
+  }
+
+  function formatTime() {
+    let hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`;
+  }
+
   return (
     <div className="clock-container">
       <div className="clock">
-        <span>00:00:00</span>
+        <span>{formatTime()}</span>
       </div>
     </div>
   );
