cb33a6e813579bd8a9a91a7e2a9413a0772e2b33
payamtajdivand <payamt1919@gmail.com>
2026-02-08 17:56:33 +0330

    add another useContext to ComponentC

--- src/ComponentC.jsx
+++ src/ComponentC.jsx
@@ -1,9 +1,13 @@
+import React, { useContext } from "react";
+import { UserContext } from "./ComponentA";
 import ComponentD from "./ComponentD";
 
 function ComponentC() {
+  const user = useContext(UserContext);
   return (
     <div className="box">
       <p>component C</p>
+      <p>hello again {user}</p>
       <ComponentD />
     </div>
   );
