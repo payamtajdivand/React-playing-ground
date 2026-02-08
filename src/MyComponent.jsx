67cc27649370b64d76760dca0b436608bfd4e8ce
payamtajdivand <payamt1919@gmail.com>
2026-02-08 19:04:04 +0330

    aad two more blocks into the app and click each button only trigges the exact input none the others and also avoid ReRender component and triggerind the useEffect() hook

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,18 +1,56 @@
 import React, { useState, useRef, useEffect } from "react";
 
 function MyComponent() {
-  const ref = useRef(0);
+  const inputRef1 = useRef(null);
+  const inputRef2 = useRef(null);
+  const inputRef3 = useRef(null);
 
   useEffect(() => {
     console.log("Componet Rendered");
   });
 
-  function handleClick() {
-    ref.current++;
-    console.log(ref.current);
+  function handleClick1() {
+    inputRef1.current.style.backgroundColor = "hsla(200, 80%, 50%, 0.5)";
+    inputRef2.current.style.backgroundColor = "";
+    inputRef3.current.style.backgroundColor = "";
+
+    inputRef1.current.focus;
+  }
+  function handleClick2() {
+    inputRef2.current.style.backgroundColor = "hsla(200, 80%, 50%, 0.5)";
+    inputRef1.current.style.backgroundColor = "";
+    inputRef3.current.style.backgroundColor = "";
+
+    inputRef2.current.focus;
   }
 
-  return <button onClick={handleClick}>Click me</button>;
+  function handleClick3() {
+    inputRef3.current.style.backgroundColor = "hsla(200, 80%, 50%, 0.5)";
+    inputRef2.current.style.backgroundColor = "";
+    inputRef1.current.style.backgroundColor = "";
+
+    inputRef3.current.focus;
+  }
+
+  return (
+    <div>
+      <>
+        <button onClick={handleClick1}>Click me</button>
+        <input type="text" ref={inputRef1} />
+      </>
+      <br />
+      <>
+        <button onClick={handleClick2}>Click me</button>
+        <input type="text" ref={inputRef2} />
+      </>
+      <br />
+
+      <>
+        <button onClick={handleClick3}>Click me</button>
+        <input type="text" ref={inputRef3} />
+      </>
+    </div>
+  );
 }
 
 export default MyComponent;
