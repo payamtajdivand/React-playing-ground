b5f45167f7b4c518dd5f7893cbcfe491bdf3b15c
payamtajdivand <payamt1919@gmail.com>
2026-02-08 18:31:22 +0330

    useRef() addition or EXAMPLE 2

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useRef, useEffect } from "react";
 
 function MyComponent() {
-  let [count, setCount] = useState(0);
+  const ref = useRef(0);
 
   useEffect(() => {
     console.log("Componet Rendered");
   });
 
   function handleClick() {
-    setCount((c) => c + 1);
+    ref.current++;
+    console.log(ref.current);
   }
 
   return <button onClick={handleClick}>Click me</button>;
